d14e8252f9b2023cfbb49d5bafaea78740c1f574
Galang Yoga Pratama <galang058@gmail.com>
2025-05-27 06:50:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const cors = require('cors')
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const serverless = require('serverless-http');
+const connectDB = require('../config/db');
 
-const PORT = process.env.PORT || 3000
-const connectDB = require('./config/db')
-connectDB()
+const app = express();
+
+connectDB();
 
 const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-}
+  origin: process.env.ALLOWED_CLIENTS?.split(',') || ['*']
+};
+
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
 
-app.use(express.json())
-app.use(express.static('public'))
-app.set('views', path.join(__dirname, '/views'))
-app.set('view engine', 'ejs')
+// Routes
+app.use('/', require('../routes/home'));
+app.use('/uploaded-files', require('../routes/listing'));
+app.use('/cleanup', require('../routes/cleanup'));
+app.use('/api/files', require('../routes/files'));
+app.use('/files', require('../routes/filePreview'));
+app.use('/files/download', require('../routes/download'));
 
-app.use('/', require('./routes/home'))
-app.use('/uploaded-files', require('./routes/listing'))
-app.use('/cleanup', require('./routes/cleanup'))
-app.use('/api/files', require('./routes/files'))
-app.use('/files', require('./routes/filePreview'))
-app.use('/files/download', require('./routes/download'))
-
-app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}`)
-});
+// Export sebagai serverless handler
+module.exports = serverless(app);
