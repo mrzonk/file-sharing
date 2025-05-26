f342fc7141edb3ec0ff3f8929825e011db46b6fd
Galang Yoga Pratama <galang058@gmail.com>
2025-05-27 05:47:17 +0700

    Update wrangler.js

--- wrangler.js
+++ wrangler.js
@@ -1,8 +1,16 @@
 {
-  "name": "worker-name",
-  "compatibility_date": "2025-05-26",
+  "$schema": "https://unpkg.com/@cloudflare/wrangler@latest/config.schema.json",
+  "name": "nama-worker-anda",
   "main": "server.js",
-  "assets": {
-    "directory": "./dist"
+  "compatibility_date": "2024-05-27",
+  "compatibility_flags": [
+    "nodejs_compat"
+  ],
+  "vars": {
+    "ENV_VARIABLE_NAME": "YOUR_VALUE"
+  },
+  "site": {
+    "bucket": "./dist",
+    "entry-point": "worker"
   }
 }
