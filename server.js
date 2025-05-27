b314a3114956df6a9d8bf5ff52e9c9bf05de5cff
Galang Yoga Pratama <galang058@gmail.com>
2025-05-27 07:01:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const serverless = require('serverless-http');
-const connectDB = require('../config/db');
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
 
-const app = express();
-connectDB();
+const PORT = process.env.PORT || 3000
+const connectDB = require('./config/db')
+connectDB()
 
 const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS?.split(',') || '*'
-};
-
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+}
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.static('public'));
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'ejs');
 
-app.use('/', require('../routes/home'));
-app.use('/uploaded-files', require('../routes/listing'));
-app.use('/cleanup', require('../routes/cleanup'));
-app.use('/api/files', require('../routes/files'));
-app.use('/files', require('../routes/filePreview'));
-app.use('/files/download', require('../routes/download'));
+app.use(express.json())
+app.use(express.static('public'))
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
 
-module.exports = serverless(app);
+app.use('/', require('./routes/home'))
+app.use('/uploaded-files', require('./routes/listing'))
+app.use('/cleanup', require('./routes/cleanup'))
+app.use('/api/files', require('./routes/files'))
+app.use('/files', require('./routes/filePreview'))
+app.use('/files/download', require('./routes/download'))
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`)
+});
